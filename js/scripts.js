41efc854061192442791615739368525b43b4a87
Prakom <ariefirawant@gmail.com>
2024-03-28 09:18:35 +0700

    update navbar

--- js/scripts.js
+++ js/scripts.js
@@ -26,32 +26,32 @@ $(function () {
         arrows: false
     });
 
-    // // toggle scroll menu
-    // var scrollTop = 0;
-    // $(window).scroll(function () {
-    //     var scroll = $(window).scrollTop();
-    //     //adjust menu background
-    //     if (scroll > 0) {
-    //         if (scroll > scrollTop) {
-    //             $('.smart-scroll').addClass('scrolling').removeClass('up');
-    //         } else {
-    //             $('.smart-scroll').addClass('up');
-    //         }
-    //     } else {
-    //         // remove if scroll = scrollTop
-    //         $('.smart-scroll').removeClass('scrolling').removeClass('up');
-    //     }
+    // toggle scroll menu
+    var scrollTop = 0;
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        //adjust menu background
+        if (scroll > 0) {
+            if (scroll > scrollTop) {
+                $('.smart-scroll').addClass('scrolling').removeClass('up');
+            } else {
+                $('.smart-scroll').addClass('up');
+            }
+        } else {
+            // remove if scroll = scrollTop
+            $('.smart-scroll').removeClass('scrolling').removeClass('up');
+        }
 
-    //     scrollTop = scroll;
+        scrollTop = scroll;
 
         // adjust scroll to top
-        // if (scroll >= 0) {
-        //     $('.scroll-top').addClass('active');
-        // } else {
-        //     $('.scroll-top').removeClass('active');
-        // }
-        // return false;
-    // });
+        if (scroll >= 0) {
+            $('.scroll-top').addClass('active');
+        } else {
+            $('.scroll-top').removeClass('active');
+        }
+        return false;
+    });
 
     // // scroll top top
     // $('.scroll-top').click(function () {
